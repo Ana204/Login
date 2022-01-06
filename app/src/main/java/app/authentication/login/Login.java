0ac3fc06953cce920e2220013944990a672710f0
ana204 <anagomes.lucia@hotmail.com>
2022-01-06 11:39:04 -0300

    Going to profile screen

--- app/src/main/java/app/authentication/login/Login.java
+++ app/src/main/java/app/authentication/login/Login.java
@@ -1,15 +1,34 @@
 package app.authentication.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.concurrent.ExecutionException;
 
 public class Login extends AppCompatActivity {
 
     TextView txt_tela_cadastro;
+    EditText edit_email, edit_password;
+    Button btn_entrar;
+    ProgressBar progressbar;
+
+    String[] mensagens = {"Preencha todos os campo"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +39,6 @@ public class Login extends AppCompatActivity {
 
         startLoginScreen();
 
-    }
-
-    private void startLoginScreen(){
-        txt_tela_cadastro = findViewById(R.id.txt_tela_cadastro);
-
         txt_tela_cadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +47,70 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = edit_email.getText().toString();
+                String senha = edit_password.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_LONG);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else {
+                    AuthenticateUser(view);
+                }
+            }
+        });
+    }
+
+    private void AuthenticateUser(View view){
+        String email = edit_email.getText().toString();
+        String senha = edit_password.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+                    progressbar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            PerfilScreen();
+                        }
+                    },3000);
+                }else {
+                    String error;
+
+                    try {
+                        throw task.getException();
+                    }catch (Exception e){
+                        error = "Error ao logar";
+                    }
+                    Snackbar snackbar = Snackbar.make(view, error, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+
+    private void PerfilScreen(){
+       Intent intent = new Intent(Login.this, Perfil.class);
+       startActivity(intent);
+       finish();
+    }
+
+    private void startLoginScreen(){
+        txt_tela_cadastro = findViewById(R.id.txt_tela_cadastro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_password = findViewById(R.id.edit_password);
+        btn_entrar = findViewById(R.id.btn_entrar);
+        progressbar = findViewById(R.id.progressbar);
     }
 }
