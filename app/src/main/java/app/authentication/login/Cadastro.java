c9ba4bbc4b5103512da012d7b1435fd5505d5f12
ana204 <anagomes.lucia@hotmail.com>
2022-01-06 10:43:24 -0300

    save user data_base

--- app/src/main/java/app/authentication/login/Cadastro.java
+++ app/src/main/java/app/authentication/login/Cadastro.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -21,6 +32,7 @@ public class Cadastro extends AppCompatActivity {
     Button buttonCadastrar;
 
     String[] mensagens = {"Preencha todos os campos", "Cadastro realizado com sucesso"};
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,11 +72,63 @@ public class Cadastro extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if (task.isSuccessful()){
+
+                    SalveUserData();
+
                     Snackbar snackbar = Snackbar.make(view, mensagens[1], Snackbar.LENGTH_LONG);
                     snackbar.setBackgroundTint(Color.WHITE);
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
+                }else {
+                    String error;
+
+                    try {
+
+                        throw task.getException();
+
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        error = "Digite uma senha com no mínimo 6 caracteres";
+
+                    }catch (FirebaseAuthUserCollisionException e){
+                        error = "Está conta já foi cadastrada";
+
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        error = "E-mail inválido";
+
+                    }catch (Exception e){
+                        error = "Erro ao cadastrar usuário";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(view, error, Snackbar.LENGTH_LONG);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
                 }
+            }
+        });
+    }
+
+    //save user data base
+    private void SalveUserData(){
+        String nome = editNome.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("nome", nome);
+
+        userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(userID);
+        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db", "SUCESSO AO SALVAR DADOS");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_ERROR", "FALHA AO SALVAR DADOS" + e.toString());
             }
         });
     }
